73f04c374005943a08904266e008e043fa930683
fitzr <fitzroy.t.hagi@gmail.com>
2020-02-13 10:39:48 +0900

    lint fix

--- bin/led-backend.ts
+++ bin/led-backend.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { LedBackendStack } from '../lib/led-backend-stack';
+import 'source-map-support/register'
+import * as cdk from '@aws-cdk/core'
+import { LedBackendStack } from '../lib/led-backend-stack'
 
-const app = new cdk.App();
-new LedBackendStack(app, 'LedBackendStack');
+const app = new cdk.App()
+new LedBackendStack(app, 'LedBackendStack')
