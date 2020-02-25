dcd1b886959241442743dd1bd71823e4934ff82f
fitzr <fitzroy.t.hagi@gmail.com>
2020-02-25 22:29:42 +0900

    add client id

--- samples/device/mqtt-trial/subscribe.sh
+++ samples/device/mqtt-trial/subscribe.sh
@@ -4,6 +4,7 @@ mosquitto_sub --cafile .tmp/test-root-CA.crt \
               --cert .tmp/test-thing-certificate.pem.crt \
               --key .tmp/test-thing-private.pem.key \
               -h $END_POINT \
+              -i TestThing \
               -p 8883 \
               -q 1 \
               -t '$aws/things/TestThing/shadow/update/delta' \
